030617db60aa16c2d36652a1f7129f0a9cf1b79f
p0lr <p0lr@p0lr.org>
2019-02-22 15:01:06 -0700

    Added Shodan queries per host - output is un-parsed JSON

--- assassin-dns.py
+++ assassin-dns.py
@@ -2,20 +2,32 @@
 
 from dnsdb_query import DnsdbClient
 import socket
+import urllib2
+import json
 
-server='https://api.dnsdb.info'
-apikey='7fe8201d3d2d75191dd50fcae2a06822381d7138005906209c04f7bf0d69c37b'
 domain = "*.arrow.com"
 
-def getDns(server, apikey, domain):
-  client = DnsdbClient(server, apikey)
+def getDns(domain):
+  server='https://api.dnsdb.info'
+  dnsdbkey='7fe8201d3d2d75191dd50fcae2a06822381d7138005906209c04f7bf0d69c37b'
+  client = DnsdbClient(server, dnsdbkey)
   output = []
   for rrset in client.query_rrset(domain):
     # rrset is a decoded JSON blob
     output.append(rrset["rrname"].rstrip('.'))
   return set(output)
 
-dns = getDns(server, apikey, domain)
+def getShodan(ip):
+  shodankey='C9tNjcBpKWoDmuqbbZ9lzeXKrv58iug8'
+  url='https://api.shodan.io/shodan/host/%s?key=%s' % (ip, shodankey)
+  try:
+    jsonresponse = urllib2.urlopen(url)
+    response = json.loads(jsonresponse.read())
+    return response  
+  except urllib2.HTTPError, e:
+    pass
+
+dns = getDns(domain)
 if (len(dns) > 0):
   for host in dns:
     try:
@@ -29,6 +41,9 @@ if (len(dns) > 0):
           try:
             reversedns = socket.getfqdn(ip)
             print "\t%s: %s" % (ip, reversedns)
+            shodan = getShodan(ip)
+            if (shodan is not None):
+              print shodan
           except socket.gaierror, err:
             print "\t%s" % (ip,)
     except socket.gaierror, err:
