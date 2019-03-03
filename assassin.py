eb319e8731b2fe8138b55ef38a1257ce5f00d094
p0lr <p0lr@p0lr.org>
2019-03-03 13:31:54 -0700

    Started improving hosting detection and comments

--- assassin.py
+++ assassin.py
@@ -7,11 +7,14 @@ import urllib2
 import json
 from sys import stdout
 
+#Let's grab a domain to analyze
 domain = raw_input("What domain would you like to search? ")
 
+#These will be used in the summary report generated at the end of the script
 dnsnames = 0
 livehosts = 0
 privateips = 0
+hosting = {}
 liveservices = 0
 vulnerabilities = 0
 severitynone = 0
@@ -19,13 +22,6 @@ severitylow = 0
 severitymedium = 0
 severityhigh = 0
 severitycritical = 0
-amazon = 0
-azure = 0
-google = 0
-oracle = 0
-digitalocean = 0
-rackspace = 0
-akamai = 0
 sslservices = 0
 expiredcerts = 0
 wildcardcert = 0
@@ -64,8 +60,6 @@ def checkPrivate(ip):
   unicodeip = unicode(str(ip), "utf-8")
   if (ipaddress.ip_address(unicodeip)):
     if (ipaddress.ip_address(unicodeip).is_private):
-      global privateips
-      privateips += 1
       return True
     else:
       return False
@@ -114,192 +108,215 @@ def getCve(cve):
   except urllib2.HTTPError, e:
     pass
 
+# Get DNS data for domain from HackerTarget
 dns = getDns(domain)
+
+#Make sure we get a result
 if (len(dns) > 0):
+
+  #This will be used in the summary report
   dnsnames = (len(dns) + 1)
+
+  #This gives us a nice status indicator on the CLI so the operator knows the script is still running
   dnscounter = 1  
   for host in dns:
     stdout.write("\r" + str(dnscounter) + " out of " + str(dnsnames) + " DNS entries analyzed")
     stdout.flush()
     dnscounter += 1
+    report += '<div class="host">%s</div>\n' % (host, )
+
+    #DNS resolve the hostname (This should probably be moved to a function)
     try:
       resolve = socket.gethostbyname_ex(host)
-      report += '<div class="host">%s</div>\n' % (host, )
+
+      #Check for alias resolution: just adding the data to the report for now - not recursively resolving these names
       if (len(resolve[1]) > 0):
         report += '<div class="hostinfo">\n'
         for alias in resolve[1]:
           report += "Alias: %s<br>\n" % (alias,)
+        report += "</div>\n"
+
+      #Check for IP addresses in DNS resolution
       if (len(resolve[2]) > 0):
-        livehosts = livehosts + 1
+
+        #This is used in the summary report
+        livehosts += 1
+
         for ip in resolve[2]:
+
+          #We don't want to process the same IP address over and over - check to see if IP has already been processed
           if (ip in ipaddresses):
-            report += "IP Address: %s - Already analyzed<br>\n" % (ip, )
+            report += '<div class="hostinfo">IP Address: %s - Already analyzed</div>\n' % (ip, )
+
           else:
+            #Add IP address to the list of processed addresses
             ipaddresses.append(ip)
-            if (not checkPrivate(ip)):
-              isamazon = False
-              isazure = False
-              isgoogle = False
-              isoracle = False
-              isdigitalocean = False
-              israckspace = False
-              isakamai = False
+
+            report += '<div class="hostinfo">\n'
+            report += "IP Address: %s<br>\n" % (ip, )
+
+            #Check to see if the IP address is private
+            if (checkPrivate(ip)):
+              #The IP address is private - increment the private P address counter for the summary
+              privateips += 1
+
+            else:
+              #The IP address is public - time to set some variables and try to figure out who is hosting these IPs
+
+              #Some hosting services include their identity in reverse DNS information
               try:
                 reversedns = socket.getfqdn(ip)
-                if("amazon" in reversedns) or ("cloudfront" in reversedns):
-                  isamazon = True
-                if("azure" in reversedns):
-                  isazure = True
-                if("google" in reversedns):
-                  isgoogle = True
-                if("akamai" in reversedns):
-                  isakamai = True
-                report += "IP Address: %s<br>\n" % (ip,)
-                report += "Reverse DNS: %s<br>\n" % (reversedns,)
-                try:
-                  whoisclient = IPWhois(str(ip))
-                  whoisresult = whoisclient.lookup_rdap(depth=4)
-                  if (whoisresult.has_key("org_name")):
-                    whois = whoisresult["org_name"]
-                    if ("Amazon.com" in whois):
-                      isamazon = True
-                    if ("Microsoft Corporation" in whois):
-                      isazure = True
-                    if ("Google" in whois):
-                      isgoogle = True
-                    if ("Oracle Corporation" in whois):
-                      isoracle = True
-                    report += "Whois: %s<br>\n" % (whois, )
-                  elif (whoisresult.has_key("asn_description")):
-                    whois = whoisresult["asn_description"]
-                    if ("Amazon.com" in whois):
-                      isamazon = True
-                    if ("Microsoft Corporation" in whois):
-                      isazure = True
-                    if ("Google" in whois):
-                      isgoogle = True
-                    if ("Oracle Corporation" in whois):
-                      isoracle = True
-                    report += "Whois: %s<br>\n" % (whois, )
-                except:
-                  pass
-                shodan = getShodan(ip)
-                if (shodan is not None):
-                  if shodan.has_key("vulns"):
-                    for vuln in shodan["vulns"]:
-                      vulnerabilities += 1  
-                      cvedata = getCve(vuln)
-                      report += "<br><font face=courier size=2>Vulnerability: %s - %s - %s</font><br>\n" % (vuln, cvedata["cvss"], cvedata["severity"], )
-                      report += "<font face=courier size=1>%s</font><br>\n" % (cvedata["summary"], )
-                  if shodan.has_key("org"):
-                    if (shodan["org"] is not None):
-                      report += "<font face=courier size=2>Org: %s</font><br>\n" % (shodan["org"],)
-                      if ("Amazon" in shodan["org"]):
-                        isamazon = True
-                      if ("Microsoft Azure" in shodan["org"]):
-                        isazure = True
-                      if ("Google Cloud" in shodan["org"]):
-                        isgoogle = True
-                      if ("Oracle" in shodan["org"]):
-                        isoracle = True
-                      if ("Digital Ocean" in shodan["org"]):
-                        isdigitalocean = True
-                      if ("Rackspace" in shodan["org"]):
-                        israckspace = True
+
+                #When reverse DNS does not find an entry, it just returns the IP address.
+                if (reversedns != ip):
+
+                  report += "Reverse DNS: %s<br>\n" % (reversedns,)
+
+              #What to do if there is a DNS resolution error
+              except:
+                pass
+
+              #WhoIs should also give a clue about the hosting provider
+              try:
+                whoisclient = IPWhois(str(ip))
+                whoisresult = whoisclient.lookup_rdap(depth=4)
+
+                #The whois client returns data in two useful areas, either org_name or asn_description.  We're checking for ogr_name first
+                if (whoisresult.has_key("org_name")):
+                  whois = whoisresult["org_name"]
+
+                  #add the whois data into the report
+                  report += "Whois: %s<br>\n" % (whois, )
+
+                  if hosting.has_key(whois):
+                    hosting[whois] += 1
                   else:
-                    report += "</div>\n"
-                  if shodan.has_key("data"):
-                    for service in shodan["data"]:
-                      serviceport = ""
-                      if service.has_key("transport") and service.has_key("port"):
-                        report += '<div class="serviceport">%s/%s</div>\n' % (service["transport"], service["port"])
-                      if service.has_key("data"):
-                        liveservices = liveservices + 1
-                        report += "<xmp>"
-                        data = service["data"].split("\n")                      
-                        for rawline in data:
-                          line = rawline.encode('ascii', 'ignore').decode('ascii').strip()
-                          if (len(line) > 0):
-                            if ("HTTP" in line):
-                              httplisteners = httplisteners + 1
-                            if ("HTTP/1.1 200 OK" in line):
-                              http200s = http200s + 1
-                            if ("SSH" in line):
-                              sshlisteners = sshlisteners + 1
-                            report += line + "\n"
-                        report += "</xmp>\n"  
-                      if service.has_key("ssl"):
-                        sslservices += 1
-                        report += '<div class="ssl">SSL<br>\n'
-                        if service["ssl"].has_key("cert"):
-                          if service["ssl"]["cert"].has_key("expired"):
-                            if (service["ssl"]["cert"]["expired"] == True):
-                              expiredcerts += 1
-                            report += "Expired: %s<br>\n" % (service["ssl"]["cert"]["expired"], )
-                          if service["ssl"]["cert"].has_key("issuer"):
-                            if service["ssl"]["cert"]["issuer"].has_key("O"):
-                              report += "Issuer: %s<br>\n" % (service["ssl"]["cert"]["issuer"]["O"], )
-                          if service["ssl"]["cert"].has_key("subject"):
-                            if service["ssl"]["cert"]["subject"].has_key("CN"):
-                              if("*" in service["ssl"]["cert"]["subject"]["CN"]):
-                                wildcardcert += 1
-                              report += "Common Name: %s<br>\n" % (service["ssl"]["cert"]["subject"]["CN"], )
-                        report += "</div>\n"
+                    hosting[whois] = 1
+
+                  #If the org_name is not returned, we should check for asn_description
+                elif (whoisresult.has_key("asn_description")):
+                  whois = whoisresult["asn_description"]
+
+                  #Add asn_description into the report
+                  report += "Whois: %s<br>\n" % (whois, )
+
+                  if hosting.has_key(whois):
+                    hosting[whois] += 1
+                  else:
+                    hosting[whois] = 1
+
+              #What to do if whois lookup fails
+              except:
+                pass
+
+              shodan = getShodan(ip)
+              if (shodan is not None):
+                if shodan.has_key("vulns"):
+                  for vuln in shodan["vulns"]:
+                    vulnerabilities += 1  
+                    cvedata = getCve(vuln)
+                    report += "<br><font face=courier size=2>Vulnerability: %s - %s - %s</font><br>\n" % (vuln, cvedata["cvss"], cvedata["severity"], )
+                    report += "<font face=courier size=1>%s</font><br>\n" % (cvedata["summary"], )
+                if shodan.has_key("org"):
+                  if (shodan["org"] is not None):
+                    report += "<font face=courier size=2>Org: %s</font><br>\n" % (shodan["org"],)
                 else:
                   report += "</div>\n"
-                if (isamazon):
-                  amazon += 1
-                if (isazure):
-                  azure += 1
-                if (isgoogle):
-                  google += 1
-                if (isoracle):
-                  oracle += 1
-                if (isdigitalocean):
-                  digitalocean += 1
-                if (israckspace):
-                  rackspace += 1
-                if (isakamai):
-                  akamai += 1
-              except socket.gaierror, err:
-                report += "<font face=courier size=2>%s</font><br>\n" % (ip,)
+                if shodan.has_key("data"):
+                  for service in shodan["data"]:
+                    serviceport = ""
+                    if service.has_key("transport") and service.has_key("port"):
+                      report += '<div class="serviceport">%s/%s</div>\n' % (service["transport"], service["port"])
+                    if service.has_key("data"):
+                      liveservices = liveservices + 1
+                      report += "<xmp>"
+                      data = service["data"].split("\n")                      
+                      for rawline in data:
+                        line = rawline.encode('ascii', 'ignore').decode('ascii').strip()
+                        if (len(line) > 0):
+                          if ("HTTP" in line):
+                            httplisteners = httplisteners + 1
+                          if ("HTTP/1.1 200 OK" in line):
+                            http200s = http200s + 1
+                          if ("SSH" in line):
+                            sshlisteners = sshlisteners + 1
+                          report += line + "\n"
+                      report += "</xmp>\n"  
+                    if service.has_key("ssl"):
+                      sslservices += 1
+                      report += '<div class="ssl">SSL<br>\n'
+                      if service["ssl"].has_key("cert"):
+                        if service["ssl"]["cert"].has_key("expired"):
+                          if (service["ssl"]["cert"]["expired"] == True):
+                            expiredcerts += 1
+                          report += "Expired: %s<br>\n" % (service["ssl"]["cert"]["expired"], )
+                        if service["ssl"]["cert"].has_key("issuer"):
+                          if service["ssl"]["cert"]["issuer"].has_key("O"):
+                            report += "Issuer: %s<br>\n" % (service["ssl"]["cert"]["issuer"]["O"], )
+                        if service["ssl"]["cert"].has_key("subject"):
+                          if service["ssl"]["cert"]["subject"].has_key("CN"):
+                            if("*" in service["ssl"]["cert"]["subject"]["CN"]):
+                              wildcardcert += 1
+                            report += "Common Name: %s<br>\n" % (service["ssl"]["cert"]["subject"]["CN"], )
+                      report += "</div>\n"
+              else:
+                report += "</div>\n"
+
     except socket.gaierror, err:
       pass
 
 summary = "<font face=courier size=10>\n"
-summary += "%s<br>\n" % (domain, )
-summary += "<font face=courier size=2><br>\n"
+summary += "%s\n" % (domain, )
+summary += '<div class="summarydata">\n'
 summary += "DNS Entries: %s<br>\n" % (dnsnames, )
 summary += "Live DNS Entries: %s<br>\n" % (livehosts, )
-summary += "<br><font face=courier size=3>Hostings Information<font face=courier size=2><br>\n"
+summary += "</div>\n"
+summary += '<div class="summaryheader">Hostings Information</div>\n'
+summary += '<div class="summarydata">\n'
 summary += "Total IPs Analyzed: %s<br>\n" % (len(ipaddresses), )
 summary += "Private IPs: %s<br>\n" % (privateips, )
-summary += "Amazon: %s<br>\n" % (amazon, )
-summary += "Azure: %s<br>\n" % (azure, )
-summary += "Google: %s<br>\n" % (google, )
-summary += "Oracle: %s<br>\n" % (oracle, )
-summary += "Rackspace: %s<br>\n" % (rackspace, )
-summary += "Digital Ocean: %s<br>\n" % (digitalocean, )
-summary += "Akamai: %s<br>\n" % (akamai, )
-summary += "<br><font face=courier size=3>Services<font face=courier size=2><br>\n"
+for host in hosting:
+  summary += "%s: %s<br>\n" % (host, hosting[host])
+#summary += "Amazon: %s<br>\n" % (amazon, )
+#summary += "Azure: %s<br>\n" % (azure, )
+#summary += "Google: %s<br>\n" % (google, )
+#summary += "Oracle: %s<br>\n" % (oracle, )
+#summary += "Rackspace: %s<br>\n" % (rackspace, )
+#summary += "Digital Ocean: %s<br>\n" % (digitalocean, )
+#summary += "Akamai: %s<br>\n" % (akamai, )
+summary += "</div>\n"
+summary += '<div class="summaryheader">Services<div>\n'
+summary += '<div class="summarydata">\n'
 summary += "Total: %s<br>\n" % (liveservices, )
 summary += "HTTP(S): %s<br>\n" % (httplisteners, )
 summary += "HTTP(S) 200s: %s<br>\n" % (http200s, )
 summary += "SSH: %s<br>\n" % (sshlisteners, )
 summary += "SSL: %s<br>\n" % (sslservices, )
-summary += "<br><font face=courier size=3>Certificates<font face=courier size=2><br>\n"
+summary += "</div>"
+summary += '<div class="summaryheader">Certificates</div>\n'
+summary += '<div class="summarydata">\n'
 summary += "Wildcard Certificates: %s<br>\n" % (wildcardcert, )
 summary += "Expired Certificates: %s<br>\n" % (expiredcerts, )
-summary += "<br><font face=courier size=3>Vulnerabilities<font face=courier size=2><br>\n"
+summary += "</div>"
+summary += '<div class="summaryheader">Vulnerabilities</div>\n'
+summary += '<div class="summarydata">\n'
 summary += "Total: %s<br>\n" % (vulnerabilities, )
 summary += "None: %s<br>\n" % (severitynone, )
 summary += "Low: %s<br>\n" % (severitylow, )
 summary += "Medium: %s<br>\n" % (severitymedium, )
 summary += "High: %s<br>\n" % (severityhigh, )
 summary += "Critical: %s<br>\n" % (severitycritical, )
+summary += "</div>"
 
 css = "<head>\n"
 css += "<style>\n"
+css += "div.summaryheader {\n"
+css += "\tfont: 15pt courier;\n"
+css += "}\n"
+css += "div.summarydata {\n"
+css += "\tfont: 11pt courier;\n"
+css += "\tmargin-left: 25px;\n"
+css += "}\n"
 css += "div.host {\n"
 css += "\tfont: 15pt courier;\n"
 css += "}\n"
@@ -325,6 +342,6 @@ filename = domain.split(".")[0] + ".html"
 file = open(filename, "w")
 file.write(css)
 file.write(summary)
+file.write("<br><br>\n")
 file.write(report)
 file.close
-
