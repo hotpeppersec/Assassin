838bf9cddb2a38dacce90525ca05eb434875811e
p0lr <p0lr@p0lr.org>
2019-10-24 12:02:44 -0600

    Delete apiKeys.py

--- apiKeys.py
+++
@@ -1,5 +0,0 @@
-#!/usr/dev/env python
-
-vtKey = '7b047d96daebcbe4fc683016b07c4b82580603bbfab4a7a2fc055f1b6d5a0318'
-shodanKey = 'C9tNjcBpKWoDmuqbbZ9lzeXKrv58iug8'
-GoogleMapsKey = 'AIzaSyDmc9w4TJdFk2MJpZvflVeCNfcjwf0_D1c'
