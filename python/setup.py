7647ff6753fecebc8569fa66bbeaad446c374b7f
FrankTheTank <franklin@bitsmasher.net>
2020-01-04 20:51:47 -0500

    validate JoN on commit

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='Assassin',
+      version='1.0',
+      description='',
+      author='Gregory D. Evans',
+      author_email='',
+      url=''
+     )
